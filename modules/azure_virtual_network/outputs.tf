93a6b1fb26f26f95d2f1db573f2718b96918fee6
Patryk <patryk.podlas@outlook.com>
2023-05-18 09:06:43 +0100

    update module

--- modules/azure_virtual_network/outputs.tf
+++ modules/azure_virtual_network/outputs.tf
@@ -1,3 +1,16 @@
 output "virtual_network_name" {
   value = azurerm_virtual_network.virtual_network.name
 }
+
+output "address_space" {
+  value = azurerm_virtual_network.virtual_network.address_space
+}
+
+output "dns_servers" {
+  value = azurerm_virtual_network.virtual_network.dns_servers
+}
+
+output "subnet_name" {
+  value = azurerm_virtual_network.virtual_network.subnet[*].name
+}
+
