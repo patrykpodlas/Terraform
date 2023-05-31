065fe24555f258ff25ed3dd5dff4f2cb6982ce28
Patryk <patryk.podlas@outlook.com>
2023-05-31 17:03:36 +0100

    update

--- environments/non-production/key_vault_for_terraform/main.tf
+++ environments/non-production/key_vault_for_terraform/main.tf
@@ -6,48 +6,48 @@ module "kv-ppodlas-example" {
 
 module "kv-secret-env-terra-vcenter-username" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-vcenter-username"
   secret_value      = "SuperSecretUsername" // Change the value
 }
 
 module "kv-secret-env-terra-vcenter-password" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-vcenter-password"
   secret_value      = "SuperSecretPassword" // Change the value
 }
 
 module "kv-secret-env-terra-da-username" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-da-username"
   secret_value      = "SuperSecretUsername" // Change the value
 }
 
 module "kv-secret-env-terra-da-password" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-da-password"
   secret_value      = "SuperSecretPassword" // Change the value
 }
 
 module "kv-secret-env-terra-communicator-username" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-communicator-username"
   secret_value      = "SuperSecretUsername" // Change the value
 }
 
 module "kv-secret-env-terra-communicator-password" {
   source            = "../../../modules/azure-key-vault-secrets"
-  key_vault_name    = module.kv-ppodlas-example.key-vault-name
-  key_vault_rg_name = module.kv-ppodlas-example.rg-name
+  key_vault_name    = module.kv-ppodlas-example.key_vault_name
+  key_vault_rg_name = module.kv-ppodlas-example.key_vault_rg_name
   secret_name       = "env-terra-communicator-password"
   secret_value      = "SuperSecretPassword" // Change the value
 }
